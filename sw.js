964dee17af889994bd97ffe63e4e368659126631
MarcosJuarez25 <marcosemanueljuarez@hotmail.com>
2026-09-11 14:52:16 -0300

    Add push and notificationclick handlers to service worker

--- sw.js
+++ sw.js
@@ -15,3 +15,30 @@ self.addEventListener("fetch", function(event) {
     })
   );
 });
+
+self.addEventListener("push", function(event) {
+  var data = {};
+  try { data = event.data ? event.data.json() : {}; } catch (e) {}
+  var title = data.title || "Family";
+  var options = {
+    body: data.body || "Hay un nuevo movimiento",
+    icon: "./icon-192.png",
+    badge: "./icon-192.png",
+    tag: "family-app-update",
+    renotify: true,
+    data: { url: data.url || "./" }
+  };
+  event.waitUntil(self.registration.showNotification(title, options));
+});
+
+self.addEventListener("notificationclick", function(event) {
+  event.notification.close();
+  event.waitUntil(
+    clients.matchAll({ type: "window", includeUncontrolled: true }).then(function(clientList) {
+      for (var i = 0; i < clientList.length; i++) {
+        if ("focus" in clientList[i]) return clientList[i].focus();
+      }
+      if (clients.openWindow) return clients.openWindow((event.notification.data && event.notification.data.url) || "./");
+    })
+  );
+});
